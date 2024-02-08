2dd9e6d532ad17ddb9b94b6a31e90cf17ff75f25
wwlouis00 <wwlouis00@gmail.com>
2024-02-08 22:03:23 +0800

    update medical

--- predict.py
+++ predict.py
@@ -10,8 +10,3 @@ model.info()
 # predict your image or video
 results = model.predict(source="your_image",project="your_save_dir",name="your_folder_name",save=True)
 print(results)
-
-# Detecting images(.jpg or .png) in an entire folder.
-for image_path in glob.glob(f'your_image/*jpg'):
-      results = model.predict(source=image_path,project="your_save_dir",name="your_folder_name",save=True)
-      print("\n")
